a4d0484474bf42ad9a905750b0cf8d480e8d38c7
Florian <git@florian-a.dev>
2024-05-04 18:01:16 +0200

    J'espère qu'on va dormir cette nuit

--- back/server.js
+++ back/server.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 const indexRoute = require('./routes/index');
 const helloRoute = require('./routes/hello');
+const freeRoute = require('./routes/free');
+const mintRoute = require('./routes/mint');
 
 app.use('/', indexRoute);
 app.use('/hello', helloRoute);
+app.use('/free', freeRoute);
+app.use('/mint', mintRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
