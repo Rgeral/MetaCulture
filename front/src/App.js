b01a35ddaefaa52438935027188f25c80598109d
Florian <git@florian-a.dev>
2024-05-04 14:09:18 +0200

    Add react-routeur

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Importez BrowserRouter, Routes et Route
+import MintNFT from './MintNFT'; // Importez votre composant de page de test
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/mint-nft" element={<MintNFT />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ front/src/MintNFT.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MintNFT() {
+    return (
+        <p>Ceci est la page de test !</p>
+    );
+}
+
+export default MintNFT;
