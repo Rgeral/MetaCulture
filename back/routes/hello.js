3de6953473de371d3fc3d8aa9092ded60f9facc7
Florian <git@florian-a.dev>
2024-05-04 17:01:18 +0200

    Add backend exemples

--- back/routes/hello.js
+++ back/routes/hello.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Route GET pour la racine
 router.get('/', (req, res) => {
-    res.send('Index');
+    res.send('Hello !');
 });
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const indexRoute = require('./routes/index');
+const helloRoute = require('./routes/hello');
 
 app.use('/', indexRoute);
+app.use('/hello', helloRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
