287b924ffdfc5cbfd2c80d07af209f00533c6a87
Florian <git@florian-a.dev>
2024-05-04 15:01:08 +0200

    Bug talwind css

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Test from './Test'; 
-import MintNFT from './MintNFT'; 
-
+import Test from './Test';
+import MintNFT from './MintNFT';
 import './App.css';
 
 function App() {
