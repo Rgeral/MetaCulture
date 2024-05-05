9aa21d0cf198867fa5751944ed3bdcb2ca9e943c
Florian <git@florian-a.dev>
2024-05-05 12:10:36 +0200

    Add docker and database

--- back/server.js
+++ back/server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const indexRoute = require('./routes/index');
-const helloRoute = require('./routes/hello');
-const freeRoute = require('./routes/free');
-const mintRoute = require('./routes/mint');
-const moveRoute = require('./routes/move');
-const acceptRoute = require('./routes/accept');
+const indexRoute = require('./src/index');
+const helloRoute = require('./src/hello');
+const freeRoute = require('./src/free');
+const mintRoute = require('./src/mint');
+const moveRoute = require('./src/move');
+const acceptRoute = require('./src/accept');
+const dbTestRoute = require('./src/dbtest');
 
 app.use('/', indexRoute);
 app.use('/hello', helloRoute);
@@ -16,6 +17,7 @@ app.use('/free', freeRoute);
 app.use('/mint', mintRoute);
 app.use('/move', moveRoute);
 app.use('/accept', acceptRoute);
+app.use('/dbtest', dbTestRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
