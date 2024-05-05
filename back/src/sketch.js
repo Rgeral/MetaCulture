470a76ed0fc168a03c3a848006bf77236dc8aa85
Robin <Geral.robin@yahoo.fr>
2024-05-05 14:44:05 +0200

    remove canvas

--- back/src/sketch.js
+++ back/src/sketch.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
-const { createCanvas } = require('canvas');
 const path = require('path');
 
 // Function to generate a random color
