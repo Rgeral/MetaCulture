acfaecd495023267095650c75b0f6ad1e5282f5c
Valikpp <vadimpp0424@gmail.com>
2024-05-05 12:58:46 +0200

    IPFS Sender

--- back/server.js
+++ back/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5173;
 
 const indexRoute = require('./routes/index');
 const helloRoute = require('./routes/hello');
@@ -8,6 +8,8 @@ const helloRoute = require('./routes/hello');
 app.use('/', indexRoute);
 app.use('/hello', helloRoute);
 
+const routes = app._router.stack.filter((r) => r.route).map((r) => r.route.path);
+console.log(routes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
