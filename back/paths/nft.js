d36c4777df0df6d4969cc03740de77d7f3b30d06
Florian A <git@florian-a.dev>
2024-05-16 17:23:46 +0200

    Add jwt security

--- back/paths/nft.js
+++ back/paths/nft.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const xrplClient = require('../utils/xrpl');
+const jwt = require('jsonwebtoken');
 
 // Magic link path
 router.get('/get', async (req, res) => {
 
+  const jwtSecret = process.env.JWT_SECRET;
+
   await xrplClient.connect();
 
-  try {
-      // Return response
-      return res.status(201).json({ test: "test" });
 
-  } catch (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Internal server error.' });
+  const bearerHeader = req.headers["authorization"];
+
+  console.log(bearerHeader);
+
+  const bearer = bearerHeader.split(' ');
+
+  const bearerToken = bearer[1];
+
+  try {
+    if (!bearerToken) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+    jwt.verify(bearerToken, jwtSecret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid token' });
+      } else {
+        res.status(200).json({ message: 'Token is valid', id: decoded.userId });
+      }
+    }
+  );
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
   }
+
 });
 
 module.exports = router;
