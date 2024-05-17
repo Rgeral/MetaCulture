93c5b21a3d79f246d3d970a096a682fefb5849de
Florian <git@florian-a.dev>
2024-05-17 06:27:43 +0200

    Sleep

--- back/paths/nft.js
+++ back/paths/nft.js
@@ -1,15 +1,97 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
+const xrpl = require('xrpl');
 const xrplClient = require('../utils/xrpl');
 const auth = require('../utils/auth');
+const db = require('../utils/db');
+const { createQueryAsync } = require('../utils/helpers');
+
+// Query asynchrone
+const queryAsync = createQueryAsync(db);
+
+async function createNFT() {
+
+  // Connect to the XRPL
+  await xrplClient.connect();
+
+  // Get wallet
+  const wallet = xrpl.Wallet.fromSeed(process.env.XRP_WALLET_SEED);
+
+  // Create a transaction to mint an NFT
+  const nftMintTx = {
+    "TransactionType": "NFTokenMint",
+    "Account": wallet.address,
+    "Flags": 8,
+    "NFTokenTaxon": 0,  // Zero means no special taxonomy
+    "Fee": "10",
+    "URI":  urlToHex("https://test.com"),
+  };
+
+  console.log("Submitting NFT mint transaction...");
+  const tx = await client.submitAndWait(nftMintTx, { wallet });
+
+  // Output the results
+  console.log(JSON.stringify(tx, null, 2));
+
+  // Disconnect from the client
+  await client.disconnect();
+}
 
 // Magic link path
 router.get('/get', auth, async (req, res) => {
 
-  console.log(req.decoded);
-  res.status(200).json({ message: 'Hello user:'+ req.decoded.userId });
+  try {
+    // Prepared SQL query to prevent SQL injection
+    const query1 = 'SELECT * FROM nft WHERE userId = ?';
+    const result1 = await queryAsync(query1, [req.decoded.userId]);
+
+    if (result1.length === 0) {
+      await createNFT();
+    }
+
+    console.log(result1);
+
+    console.log(req.decoded);
+    res.status(200).json({ message: 'Hello user:' + req.decoded.userId });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
+
 
 });
+
+
+
+async function createNFT() {
+
+  await xrplClient.connect();
+
+  // Your wallet seed - replace with your actual testnet wallet seed
+  const wallet = xrplClient.Wallet.fromSeed(process.env.XRP_WALLET_SEED);
+
+  const ipfs_url = await pushIpfs();
+
+  // Create a transaction to mint an NFT
+  const nftMintTx = {
+    "TransactionType": "NFTokenMint",
+    "Account": wallet.address,
+    "Flags": 8,
+    "NFTokenTaxon": 0,  // Zero means no special taxonomy
+    "Fee": "10",
+    "URI": urlToHex(ipfs_url),
+  };
+
+  console.log("Submitting NFT mint transaction...");
+  const tx = await client.submitAndWait(nftMintTx, { wallet });
+
+  // Output the results
+  console.log(JSON.stringify(tx, null, 2));
+
+  // Disconnect from the client
+  await client.disconnect();
+}
+
 
 module.exports = router;
--- back/utils/helpers.js
+++ back/utils/helpers.js
@@ -1,19 +1,18 @@
 const crypto = require('crypto');
 const util = require('util');
 
-// Créer un hash unique
+// Generate unique hash
 function generateUniqueHash(input) {
     const hash = crypto.createHash('sha256');
     hash.update(input);
     return hash.digest('hex');
 }
 
-// Créer une version asynchrone de `db.query`
+// Asynchrone db query
 function createQueryAsync(db) {
     return util.promisify(db.query).bind(db);
 }
 
-// Exporter les fonctions
 module.exports = {
     generateUniqueHash,
     createQueryAsync
